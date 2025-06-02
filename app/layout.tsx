37f2b860051c03d5d35953d4579bf82e1c759545
Bidemi Ajala <bidemiajala@gmail.com>
2025-06-02 12:52:36 +0100

    update seo description

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     default: 'Demi - Quality Engineer',
     template: '%s | Demi'
   },
-  description: 'Quality engineer in New York.',
+  description: 'AI Quality engineer based in New York.',
   icons: {
     icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌍</text></svg>',
   },
